5b9d244d6f624e3ad2f2e466865166fa9116819e
invinciblejai <mailjai.vardhan@gmail.com>
2017-08-23 16:41:49 +0530

    updates extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,46 @@
 
 import * as vscode from 'vscode';
 
+import * as path from 'path';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 export function activate(context: vscode.ExtensionContext) {
+
+  /******************* START::  LSP client ***********************/
+
+  // The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	// The debug options for the server
+	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
+
+  let lastTagged = context.globalState.get('lastTagged', '');
+    if(lastTagged) {
+       process.env['RECOMMENDER_API_TOKEN'] = lastTagged;
+    }
+	
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	}
+	
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents 'plaintext','xml','json'
+		documentSelector: [],
+		synchronize: {
+			// Synchronize the setting section 'languageServerExample' to the server
+			configurationSection: 'languageServerExample',
+			// Notify the server about file changes to '.clientrc files contain in the workspace
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	}
+	
+	// Create the language client and start the client.
+	let disposableLSp = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions).start();
+	
+  /******************* END ::  LSP client ***********************/
 
 	let previewUri = vscode.Uri.parse('fabric8-analytics-widget://authority/fabric8-analytics-widget');
 
@@ -569,7 +607,7 @@ let render_stack_iframe = (sa) => {
 	const request = require('request');
 	let stack_analysis_requests = new Map<String, String>();
 	let stack_analysis_responses = new Map<String, String>();
-  const STACK_API_TOKEN: string = '';
+  let STACK_API_TOKEN: string = '';
 
   const STACK_API_URL: string = "https://recommender.api.openshift.io/api/v1/stack-analyses-v2"
 
@@ -589,7 +627,7 @@ let render_stack_iframe = (sa) => {
         else {
             if (httpResponse.statusCode == 202) {
                 vscode.window.showInformationMessage('Analysis in progress ...');
-                setTimeout(() => { stack_collector(file_uri, id, cb); }, 10000);
+                setTimeout(() => { stack_collector(file_uri, id, cb); }, 5000);
             }
         }
       } else {
@@ -599,13 +637,13 @@ let render_stack_iframe = (sa) => {
     });
 	};
 
-	let get_stack_metadata = (file_uri, context, cb) => {
+	let get_stack_metadata = (file_uri, contextData, cb) => {
     // if (file_uri in stack_analysis_requests) {
     //     return;
     // }
-    console.log("in here");
     let manifest_array: any = ["requirements.txt","package.json","pom.xml"];
     let manifest_mime_type: any = {"requirements.txt" : "text/plain","package.json" : "application/json" ,"pom.xml" : "text/xml"};
+    let thatContext: any;
 
     let file_uri_formatted: string = file_uri._formatted;
     let file_uri_split = file_uri_formatted.split("/");
@@ -615,18 +653,20 @@ let render_stack_iframe = (sa) => {
       if(manifest_array.indexOf(file_name) > -1){
          let form_data = {
           'manifest[]': [{
-                value: context.manifest,
+                value: contextData.manifest,
                 options: {
                     filename: file_name,
                     contentType: manifest_mime_type[file_name]
                 }
             }],
-            origin: context.origin || 'lsp'
+          'filePath[]': [file_uri_formatted],
+           origin: contextData.origin || 'lsp'
           };
           const options = {};
           options['uri'] = `${STACK_API_URL}`;
           options['headers'] = {'Authorization': 'Bearer ' + STACK_API_TOKEN};
 	        options['formData'] = form_data;
+          thatContext = context;
     
           request.post(options, (err, httpResponse, body) => {
           if ((httpResponse.statusCode == 200 || httpResponse.statusCode == 202)) {
@@ -640,6 +680,10 @@ let render_stack_iframe = (sa) => {
                 vscode.window.showErrorMessage(`Failed :: ${resp.error }, Status: ${httpResponse.statusCode}`);
                 cb(null);
             }
+          } else if(httpResponse.statusCode == 401){
+              thatContext.globalState.update('lastTagged', '');
+              vscode.window.showErrorMessage(`Looks like your token is not proper, kindly re-run stack analysis`);
+              cb(null);
           } else {   
             vscode.window.showErrorMessage(`Failed to trigger stack analysis, Status: ${httpResponse.statusCode}`);
             cb(null);
@@ -662,31 +706,41 @@ let render_stack_iframe = (sa) => {
 		let editor = vscode.window.activeTextEditor;
     let text = editor.document.getText();
 
-		get_stack_metadata(editor.document.uri, {manifest: text, origin: 'lsp'}, (data) => { provider.signal(previewUri, data) });
+		// get_stack_metadata(editor.document.uri, {manifest: text, origin: 'lsp'}, (data) => { provider.signal(previewUri, data) });
     provider.signalInit(previewUri,null);
 
     let answer1: string;
     let options = {
-      prompt: "Label: ",
+      prompt: "Action: ",
       placeHolder: "Please provide your auth token"
     }
 
-    vscode.window.showInputBox(options).then(value => {
-      if (!value) return;
-      answer1 = value;
-      //vscode.window.showInformationMessage("i got u "+ answer1);
-      return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'fabric8-analytics stack report').then((success) => {}, (reason) => {
+    let lastTagged = context.globalState.get('lastTagged', '');
+    if(!lastTagged) {
+      vscode.window.showInputBox(options).then(value => {
+        if (!value) return;
+        STACK_API_TOKEN = value;
+        process.env['RECOMMENDER_API_TOKEN'] = STACK_API_TOKEN;
+        context.globalState.update('lastTagged', STACK_API_TOKEN);
+        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'fabric8-analytics stack report').then((success) => {
+          get_stack_metadata(editor.document.uri, {manifest: text, origin: 'lsp'}, (data) => { provider.signal(previewUri, data) });
+          provider.signalInit(previewUri,null);
+           }, (reason) => {
+		 	    vscode.window.showErrorMessage(reason);
+        });
+      });
+  } else {
+       STACK_API_TOKEN = lastTagged;
+       process.env['RECOMMENDER_API_TOKEN'] = lastTagged;
+       return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'fabric8-analytics stack report').then((success) => {
+        get_stack_metadata(editor.document.uri, {manifest: text, origin: 'lsp'}, (data) => { provider.signal(previewUri, data) });
+        provider.signalInit(previewUri,null);
+      }, (reason) => {
 		 	  vscode.window.showErrorMessage(reason);
 		  });
-      // show the next dialog, etc.
-    });
-
-		// return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'fabric8-analytics stack report').then((success) => {}, (reason) => {
-		// 	vscode.window.showErrorMessage(reason);
-		// });
-
+    }
 	});
 
 	let highlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(0,0,0,.35)' });
-	context.subscriptions.push(disposable, registration);
+	context.subscriptions.push(disposable, registration, disposableLSp);
 }
